d578573298939cb3f6fe1e988289efbc5b880db9
Quangz <quang@kobizo.de>
2024-06-24 12:29:53 +0700

    view product image

--- src/components/product/ProductCard.js
+++ src/components/product/ProductCard.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
-import ProductImage from "./productExample.jpeg";
+import React, { useState, useEffect } from "react";
+// import ProductImage from "./productExample.jpeg";
+import defaultProductImage from "./defaultProductImage.jpg";
 import MyAxios from "../../util/MyAxios";
+import axios from "axios";
 
 //Props contains: product, setShowForm, setFormMode, setFormData
 export default function ProductCard(props) {
@@ -13,12 +15,69 @@ export default function ProductCard(props) {
   // const [product, setProduct] = useState(props.product);
   const product = props.product;
 
+  const [imageData, setImageData] = useState("");
+
+  //Get image data
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios
+        .get(`http://localhost:2000/api/products/${product.id}/image`, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+          responseType: "arraybuffer", // Yêu cầu phản hồi dưới dạng mảng byte (arraybuffer)
+        })
+        .then((res) => {
+          if (res.status === 200) {
+            const base64Image = btoa(
+              new Uint8Array(res.data).reduce(
+                (data, byte) => data + String.fromCharCode(byte),
+                ""
+              )
+            );
+            setImageData(`data:image/jpeg;base64, ${base64Image}`);
+          }
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+      // try {
+      //   const response = await axios.get(
+      //     `http://localhost:2000/api/products/${product.id}/image`,
+      //     {
+      //       headers: {
+      //         Authorization: `Bearer ${localStorage.getItem("token")}`,
+      //       },
+      //       responseType: "arraybuffer", // Yêu cầu phản hồi dưới dạng mảng byte (arraybuffer)
+      //     }
+      //   );
+
+      //   const base64Image = btoa(
+      //     new Uint8Array(response.data).reduce(
+      //       (data, byte) => data + String.fromCharCode(byte),
+      //       ""
+      //     )
+      //   );
+
+      //   setImageData(`data:image/jpeg;base64, ${base64Image}`);
+      // } catch (error) {
+      //   console.log(error);
+      // }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     product && (
       <>
         <div className="col-md-3 mb-3">
           <div className="card">
-            <img src={ProductImage} alt="Product"></img>
+            <img
+              src={imageData !== "" ? imageData : defaultProductImage}
+              height={"140"}
+              alt="Product"
+            ></img>
             <div className="card-body">
               <h6 className="card-title mb-1">{product.name}</h6>
               <div className="row mb-2 px-2">
